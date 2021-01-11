b5c05b81010cd1b8a9a5da0243eb34f2a8e72c5f
Charlie Jackson <charlieojackson@gmail.com>
2021-01-11 08:29:30 +0000

    Updates

---
+++ precache-manifest.7eaa3bc68d7115a52d7a24b54d583736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05edd96ac10d469eb33251e7d424e0b6",
+    "url": "/the-dough-shack/index.html"
+  },
+  {
+    "revision": "ec3f0e64e1631afa99c0",
+    "url": "/the-dough-shack/static/css/main.29f6544a.chunk.css"
+  },
+  {
+    "revision": "9eecf2034538ee45c577",
+    "url": "/the-dough-shack/static/js/2.7d4fb125.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/the-dough-shack/static/js/2.7d4fb125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3f0e64e1631afa99c0",
+    "url": "/the-dough-shack/static/js/main.bacc74b0.chunk.js"
+  },
+  {
+    "revision": "089bab7986dc3277b51b",
+    "url": "/the-dough-shack/static/js/runtime-main.ec8eb413.js"
+  }
+]);
